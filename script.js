629cc2f26b8c29670277e83eb50a8c5a08b4c59c
andaak <andre_aakerholm@hotmail.com>
2023-04-30 16:08:27 +0200

    Add mouseover logic

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const container = document.getElementById('container');
 function makeGrid(rows, columns) {
     for (let i = 0; i < rows * columns; i++) {
         let cell = document.createElement('div');
+        cell.style.backgroundColor = 'azure'
         container.appendChild(cell).className = 'cell';
     }
 }
 
 makeGrid(16, 16);
+
+let cells = document.querySelectorAll('.cell');
+const hoverEffect = (e) => {
+    e.target.style.transition = '150ms';
+    e.target.style.borderColor = 'red';
+    e.target.style.backgroundColor = 'darkred';
+}
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', hoverEffect, { once: true });
+})
